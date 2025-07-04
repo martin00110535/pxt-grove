b7eafb02fa45397b844834ccd7d0f2dc929c3d67
martin00110535 <martinlee2011@gmail.com>
2025-07-04 12:30:23 +0800

    Update main.ts
    128

--- main.ts
+++ main.ts
@@ -592,7 +592,7 @@ namespace grove {
     {
         let display = new TM1637();
         
-        display.buf = pins.createBuffer(8);
+        display.buf = pins.createBuffer(128);
         display.clkPin = clkPin;
         display.dataPin = dataPin;
         display.brightnessLevel = 0;
